97265b7a7377296e8bbec3f5859ac25b28bedd0b
IvanciuVlad <ivanciu.vlad@yahoo.com>
2020-10-15 12:15:43 +0300

    Added more details

--- stocks/models.py
+++ stocks/models.py
@@ -10,7 +10,13 @@ class Stock(models.Model):
     user = models.ForeignKey(User, related_name='stocks', on_delete=models.DO_NOTHING, null=True, blank=True,)
     ticker = models.CharField(max_length=4)
     last_value = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    week52High = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    week52Low = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    primary_exchange = models.CharField(max_length=10, null=True)
     company_name = models.CharField(max_length=96)
+    ytd_change = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    pe_ratio = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    market_cap = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return self.ticker
@@ -22,6 +28,12 @@ class Stock(models.Model):
         print(stock_attributes)
         self.last_value = stock_attributes['latestPrice']
         self.company_name = stock_attributes['companyName']
+        self.week52Low = stock_attributes['week52Low']
+        self.week52High = stock_attributes['week52High']
+        self.primary_exchange = stock_attributes['primaryExchange']
+        self.pe_ratio = stock_attributes['peRatio']
+        self.market_cap = stock_attributes['marketCap']
+        self.ytd_change = stock_attributes['ytdChange']
 
     def get_absolute_url(self):
         return reverse('stocks:for_user', kwargs={'username': self.user.username})
